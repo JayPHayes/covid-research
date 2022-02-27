2bc5fffc85a74bd97149249631d9c77b44d20454
Jay P <Jayp3@jayp.com>
2022-02-27 07:32:36 -0600

    2022.02.27  Power BI Login

--- src/pages/PowerBi.js
+++ src/pages/PowerBi.js
@@ -11,8 +11,7 @@ function PowerBi() {
                     className='h-screen'
                     title="CovidResearch_20220226_Mobile" 
                     width="380" 
-                    
-                    src="https://app.powerbi.com/view?r=eyJrIjoiZmFlZjk1OTgtNzA2ZS00ZDRiLTljN2ItNWZmOGVmMjhlZDdmIiwidCI6IjFkMmI5N2Y2LTMyMjgtNDFkZC1iYzk1LTQwZDE2ODU5MGU2NSIsImMiOjN9" 
+                    // src="https://app.powerbi.com/view?r=eyJrIjoiZmFlZjk1OTgtNzA2ZS00ZDRiLTljN2ItNWZmOGVmMjhlZDdmIiwidCI6IjFkMmI5N2Y2LTMyMjgtNDFkZC1iYzk1LTQwZDE2ODU5MGU2NSIsImMiOjN9" 
                     frameBorder={0}
                     allowFullScreen={true}>
                 </iframe>
@@ -24,7 +23,8 @@ function PowerBi() {
                 <iframe className=' h-screen'
                     title="CovidResearch_20220226 - Cases" 
                     width="100%"  
-                    src="https://app.powerbi.com/view?r=eyJrIjoiMDU0MWY2M2UtMGM4Yy00MWNjLTg2ZGYtYzRiNGJlODZjOTEyIiwidCI6IjFkMmI5N2Y2LTMyMjgtNDFkZC1iYzk1LTQwZDE2ODU5MGU2NSIsImMiOjN9" 
+                    src="https://app.powerbi.com/reportEmbed?reportId=32345b47-58da-4b30-a1dd-480ee66e69b8&autoAuth=true&ctid=1d2b97f6-3228-41dd-bc95-40d168590e65&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLW5vcnRoLWNlbnRyYWwtYi1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" 
+                    // src="https://app.powerbi.com/view?r=eyJrIjoiMDU0MWY2M2UtMGM4Yy00MWNjLTg2ZGYtYzRiNGJlODZjOTEyIiwidCI6IjFkMmI5N2Y2LTMyMjgtNDFkZC1iYzk1LTQwZDE2ODU5MGU2NSIsImMiOjN9" 
                     frameBorder={0}
                     allowFullScreen={true}>
                 </iframe>
